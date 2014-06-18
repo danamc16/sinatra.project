fc9c4dd3364a0a00ccdd23fabdc457ac49bd2339
Dana Chaykovsky <dana.chaykovsky@yale.edu>
2014-06-18 13:42:07 -0400

    Updated versions

--- texting.rb
+++ texting.rb
@@ -9,8 +9,9 @@ end
 
 get '/' do
 	session[:groups] ||= {}
+	session[:texts] ||= {}
 	
-	erb :'index.html', :locals => {:groups => session[:groups]}
+	erb :'index.html', :locals => {:texts => session[:texts],:groups => session[:groups]}
 end
 
 post '/' do
@@ -26,14 +27,27 @@ post '/' do
 
 
 
-	erb :'index.html', :locals => {:groups => session[:groups], :member => params[:membername], :number => params[:number], :groupname => params[:groupname]}
+	erb :'index.html', :locals => {:texts => session[:texts],:groups => session[:groups], :member => params[:membername], :number => params[:number], :groupname => params[:groupname]}
 end
 
 get '/:groupname' do
 
 
+	erb :'specificgroup.html', :locals => {:groups => session[:groups],  :groupname => params[:groupname], :button => params[:button],:text => session[:text]}
+end
+
+post '/:groupname' do
+
+	recipients = params[:recipients]
+	message = params[:message]
+	time = params[:time]
+
+	session[:text][recipients] ||= {}
+	session[:text][recipients] = [time,message]
 
 
 
-	erb :'specificgroup.html', :locals => {:groups => session[:groups], :member => params[:membername], :number => params[:number], :groupname => params[:groupname], :button => params[:button]}
+
+	erb :'specificgroup.html', :locals => {:groups => session[:groups],:member => params[:membername], :number => params[:number],:groupname => params[:groupname],:recipients => recipients,:message => message,:time => time,:text => session[:text]}
+
 end
