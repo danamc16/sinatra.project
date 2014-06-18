a8c81fbf710c9aae507f1f2c822a05cc57568a5f
jdmcpeek <david.mcpeek@yale.edu>
2014-06-18 14:33:11 -0400

    init

--- texting.rb
+++ texting.rb
@@ -30,24 +30,33 @@ post '/' do
 	erb :'index.html', :locals => {:texts => session[:texts],:groups => session[:groups], :member => params[:membername], :number => params[:number], :groupname => params[:groupname]}
 end
 
-get '/:groupname' do
+get '/:groupname' do	
+
+	# return session[params[groupname]]
+	recipients = ""
+	time = DateTime.now()
+	message = ""
+	
+	display = false
 
 
-	erb :'specificgroup.html', :locals => {:groups => session[:groups],  :groupname => params[:groupname], :button => params[:button],:text => session[:text]}
+	erb :'specificgroup.html', :locals => {:groups => session[:groups],  :groupname => params[:groupname], :button => params[:button],:texts => session[:texts], :recipients => recipients,:message => message,:time => time,:display => display}
 end
 
 post '/:groupname' do
 
+
+
 	recipients = params[:recipients]
 	message = params[:message]
 	time = params[:time]
+	session[:texts] ||= {}
+	session[:texts][recipients] ||= {}
+	session[:texts][recipients] = [time,message]
 
-	session[:text][recipients] ||= {}
-	session[:text][recipients] = [time,message]
+	display = true
 
 
-
-
-	erb :'specificgroup.html', :locals => {:groups => session[:groups],:member => params[:membername], :number => params[:number],:groupname => params[:groupname],:recipients => recipients,:message => message,:time => time,:text => session[:text]}
+	erb :'specificgroup.html', :locals => {:groups => session[:groups],:member => params[:membername], :number => params[:number],:groupname => params[:groupname],:recipients => recipients,:message => message,:time => time,:texts => session[:texts],:display => display}
 
 end
