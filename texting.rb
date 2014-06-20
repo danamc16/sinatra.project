c215939240d45c6a2a7241250d2f9d383049b485
Dana Chaykovsky <dana.chaykovsky@yale.edu>
2014-06-20 16:13:54 -0400

    Added delete function and date parameter

--- texting.rb
+++ texting.rb
@@ -82,30 +82,23 @@ post '/:groupname' do
 	subject = params[:subject]
 	message = params[:message]
 	time = params[:time]
+	date = params[:date]
 	session[:emails] ||= {}
 	session[:emails][groupname] ||= []
-	session[:emails][groupname].push([recipients,time,subject,message])
+	session[:emails][groupname].push([recipients,time,date,subject,message])
 
-	url = "https://sendgrid.com/api/mail.send.json"
-
-
-scheduler = Rufus::Scheduler.new
-
-scheduler.at '2014/06/20 14:36:00' do
+	#url = "https://sendgrid.com/api/mail.send.json"
   
-	response = HTTParty.post url, :body => {
-	  "api_user" => "jdmcpeek",
-	  "api_key" => "sendgridpro",
-	  "to" => session[:groups][groupname].values,
-	  "from" => "josedmcpeek@gmail.com",
-	  "subject" => "This is a test",
-	  "text" => message
-	}
+	#response = HTTParty.post url, :body => {
+	#  "api_user" => "jdmcpeek",
+	#  "api_key" => "sendgridpro",
+	#  "to" => session[:groups][groupname].values,
+	#  "from" => "josedmcpeek@gmail.com",
+	 # "subject" => "This is a test",
+	#  "text" => message
+	#}
 
-	response.body
-
-end
-
+	#response.body
 
 
 	erb :'specificgroup.html', :locals => {:groups => session[:groups],
@@ -116,6 +109,7 @@ end
 										   :subject => subject,
 										   :message => message,
 										   :time => time,
+										   :date => date,
 										   :emails => session[:emails],
 										   :display => display}
 
@@ -131,4 +125,35 @@ get '/:groupname/edit' do
 									:emails => session[:emails],
 									:display => display}
 
+end
+
+put '/:groupname/edit' do
+
+	groupname = params[:groupname]
+	groupname_new = params[:groupname_new]
+	session[:groups][title_new] = session[:groups].delete(title)
+
+
+
+
+	#groupname_new = params[:groupname_new]
+	#session[:groups][title_new] = session[:groups].delete(title)
+
+	#description = params[:description]
+	#vidlist = params[:vidlist].split(', ')
+	#history[title_new].replace(vidlist.push(description))
+	#redirect to('/sets/' + title_new)
+	
+	erb :'edit.html', :locals => {:groups => session[:groups],
+										   :member => params[:membername], 
+										   :email => params[:email],
+										   :groupname => params[:groupname]}
+
+end
+
+delete '/:groupname/edit' do
+	groupname = params[:groupname]
+	session[:groups].delete(groupname)
+	redirect to('/')
+	erb :setname, :locals => {:groupname => groupname}
 end
